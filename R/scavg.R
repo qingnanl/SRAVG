b73f63fda6148d0711b3b3fce4569005ade62cc9
qingnanl <qliang2016@gmail.com>
2022-01-07 22:50:15 -0600

    add

--- R/scavg.R
+++ R/scavg.R
@@ -13,12 +13,22 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-sravg <- function(object, dr_key, dr_dims, group_size, group_within, extra_meta) {
+sravg <- function(object, dr_key, dr_dims, group_size, group_within, assay = 'RNA', slot = 'counts', extra_meta) {
   object <- object
+  expr <- object@assays[[assay]]@slot
   dimred <- object@reductions[[dr_key]]@cell.embeddings[, 1:dr_dims]
   group_within <- group_within
   for (group in unique(object@meta.data$group_within)){
-    idx <- group %in% object@meta.data$group_within
+    idx <- object@meta.data$group_within %in% group
+    expr_temp <- expr[, idx]
+    dimred_temp <- dimred[idx, ]
+    K <- ncol(expr_temp)/group_size
+    cluster <- balanced_clustering(dimred, K = K, method = "centroid")
+    naidx <- which(is.na(cluster))
+    cluster <- cluster[-naidx]
+    expr_temp <- expr_temp[, -naidx]
+    dimred_temp <- dimred_temp[-naidx, ]
+    
   }
   cluster <- balanced_clustering(dimred, K = group_size, method = "centroid")
 }
