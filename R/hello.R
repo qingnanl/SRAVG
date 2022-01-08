b1733b86a68eb1ce9f673f94471c37685d69541e
qingnanl <qingnan.liang@bcm.edu>
2022-01-07 22:08:04 -0600

    add

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,12 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-sravg <- function(object, dr_key, group_size, group_within) {
-  print("Hello, world!")
+sravg <- function(object, dr_key, dr_dims, group_size, group_within, extra_meta) {
+  object <- object
+  dimred <- object@reductions[[dr_key]]@cell.embeddings[, 1:dr_dims]
+  group_within <- group_within
+  for (group in unique(object@meta.data$group_within)){
+    idx <- group %in% object@meta.data$group_within
+  }
+  cluster <- balanced_clustering(dimred, K = group_size, method = "centroid")
 }
