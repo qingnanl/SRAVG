0b4f65ed93419cbe5c538888ee816a6e83564be6
qingnanl <qliang2016@gmail.com>
2022-05-17 16:11:55 -0500

    expand to chromatin assays; changed the aggregation code

--- R/sravg.R
+++ R/sravg.R
@@ -41,6 +41,8 @@
 #' Should be among colnames(object@meta.data)
 #' @param assay Which assay to use (for averaging the expression); default is 'RNA'
 #' @param slot Which slot to use (for averaging the expression); default is 'counts'
+#' @param peak_assay Which chromatin assay to use (for averaging the expression); default is NULL
+#' @param peak_slot Which slot to use for the chromatin assay (for averaging the expression); default is NULL
 #' @param extra_meta Vector of selected colnames(object@meta.data). This is the meta data that we want to
 #' average at the same time. The meta data columns selected must be numeric.
 #' For example extra_meta = c('nCount_RNA', 'nFeature_RNA')
@@ -53,13 +55,23 @@
 #'                             group_within = "seurat_clusters",
 #'                             extra_meta = c('nCount_RNA', 'nFeature_RNA'))
 
+#' pbmc_avg <- sravg(object = pbmc3k, dr_key = "pca", dr_dims = 10, group_size = 10,
+#'                             group_within = "seurat_clusters",peak_assay = "peaks", peak_slot = "data",
+#'                             extra_meta = c('nCount_RNA', 'nFeature_RNA'))
 
 
-
-sravg <- function(object, dr_key = 'pca', dr_dims, group_size, group_within, assay = 'RNA', slot = 'counts', extra_meta) {
+sravg <- function(object, dr_key = 'pca', dr_dims, group_size, 
+                  group_within, gex_assay = 'RNA', gex_slot = 'counts', 
+                  peak_assay = NULL, peak_slot = NULL,
+                  extra_meta) {
 
   object <- object
-  expr <- GetAssayData(object, assay = assay, slot = slot)
+  expr <- GetAssayData(object, assay = gex_assay, slot = gex_slot)
+  if (!is.null(peak_assay)){
+    peak_expr <- GetAssayData(object, assay = peak_assay, slot = peak_slot)
+    chrom_assay <- object[[peak_assay]]
+  }
+  
   dimred <- object@reductions[[dr_key]]@cell.embeddings[, 1:dr_dims]
 
   # meta.data for averaging
@@ -74,13 +86,17 @@ sravg <- function(object, dr_key = 'pca', dr_dims, group_size, group_within, ass
   i <- 1
   j <- 1
   for (group in unique(object@meta.data[[group_within]])){
-
+    print(paste0("averaging ", group))
     # split expression data and dimred
     idx <- object@meta.data[[group_within]] %in% group
     expr_temp <- expr[, idx]
     dimred_temp <- dimred[idx, ]
     if (!is.null(extra_meta)){
       meta_temp <- meta[idx, ]
+    }
+    
+    if (!is.null(peak_assay)){
+      peak_expr_temp <- peak_expr[, idx]
     }
 
     # calculate K (how many clusters to search)
@@ -99,13 +115,25 @@ sravg <- function(object, dr_key = 'pca', dr_dims, group_size, group_within, ass
       if (!is.null(extra_meta)){
         meta_temp <- meta_temp[-naidx, ]
       }
+      if (!is.null(peak_assay)){
+        peak_expr_temp <- peak_expr_temp[, -naidx]
+        print(head(peak_expr_temp))
+      }
     }
 
     # aggregation and give names to aggregated cells
     agg_id <- paste0(group, "_", 1:K)#aggregated cell id
-    expr_avg <- t(aggregate(t(expr_temp), list(cluster), mean))[-1, ]
+    # expr_avg <- t(aggregate(t(expr_temp), list(cluster), mean))[-1, ]
+    expr_avg <- Matrix::t(aM2(t(expr_temp), groupings = list(cluster), fun = "mean"))
     colnames(expr_avg) <- agg_id
+    if (!is.null(peak_assay)){
+      # peak_expr_avg <- t(aggregate(t(peak_expr_temp), list(cluster), mean))[-1, ]
+      peak_expr_avg <- Matrix::t(aM2(t(peak_expr_temp), groupings = list(cluster), fun = "mean"))
+      print(head(peak_expr_avg))
+      colnames(peak_expr_avg) <- agg_id
+    }
     dimred_avg <- aggregate(dimred_temp, list(cluster), mean)[, -1]
+    # dimred_avg <- Matrix::t(aM2(t(dimred_temp), groupings = list(cluster), fun = "mean"))
     rownames(dimred_avg) <- agg_id
     if (!is.null(extra_meta)){
       meta_avg <- aggregate(meta_temp, list(cluster), mean)[, -1]
@@ -119,6 +147,15 @@ sravg <- function(object, dr_key = 'pca', dr_dims, group_size, group_within, ass
       obj_temp <- CreateSeuratObject(counts = expr_avg)
     }
     obj_temp@meta.data[[group_within]] <- group
+    
+    if (!is.null(peak_assay)){
+      chrom_assay_temp <- chrom_assay
+      chrom_assay_temp@counts <- peak_expr_avg
+      chrom_assay_temp@data <- peak_expr_avg
+      obj_temp[[peak_assay]] <- chrom_assay_temp
+      print("peak assay averaging done")
+    }
+    
 
     # put objects in list and merge later
     obj.list[[i]] <- obj_temp
@@ -140,3 +177,28 @@ sravg <- function(object, dr_key = 'pca', dr_dims, group_size, group_within, ass
   return(obj)
 }
 
+#' This aM2 function is modified from the 'aggregate.Matrix' fuction in the Matrix.utils package.
+#' The purpose of this function is to compute the averaged gene/peak levels within a group. This is way faster than the original 'aggregate' function.
+aM2<-function(x,groupings=NULL,form=NULL,fun='sum',...)
+{
+  if(!is(x,'Matrix'))
+    x<-Matrix(as.matrix(x),sparse=TRUE)
+  if(fun=='count')
+    x<-x!=0
+  groupings2<-groupings
+  if(!is(groupings2,'data.frame'))
+    groupings2<-as(groupings2,'data.frame')
+  groupings2<-data.frame(lapply(groupings2,as.factor))
+  groupings2<-data.frame(interaction(groupings2,sep = '_'))
+  colnames(groupings2)<-'A'
+  if(is.null(form))
+    form<-as.formula('~0+.')
+  form<-as.formula(form)
+  mapping<-dMcast(groupings2,form)
+  colnames(mapping)<-substring(colnames(mapping),2)
+  result<-t(mapping) %*% x
+  if(fun=='mean')
+    result@x<-result@x/(aggregate.Matrix(x+1,groupings2,fun='count'))@x
+  attr(result,'crosswalk')<-grr::extract(groupings,match(rownames(result),groupings2$A))
+  return(result)
+}
